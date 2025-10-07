f20a86a0ab245b51470f25d053250d2a1d9aeccf
Raul Cabral Caxeta <raulcabralc@gmail.com>
2025-10-07 13:16:46 -0300

    Create Pedido.java

---
+++ lista_exercicios_poo/SistemaVendas/Pedido.java
@@ -0,0 +1,31 @@
+import java.util.Date;
+
+public class Pedido {
+    private String numero;
+    private Date data;
+    private float valorTotal;
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    public float getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(float valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+}
